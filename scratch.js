48eddc1958c163788afd14fd544398efff69e689
peyo <peteryyoon@gmail.com>
2020-06-03 14:00:54 -0700

    changes to q 13 org chart based on new recurive algo.

--- scratch.js
+++ scratch.js
@@ -1,11 +1,71 @@
-const tri = (n) => {
+const facebook = {
+  Zuckerberg: {
+    Schroepfer: {
+      Bosworth: {
+        Steve: {},
+        Kyle: {},
+        Andra: {}
+      },
+      Zhao: {
+        Richie: {},
+        Sofia: {}
+      }
+    },
+    Schrage: {
+      VanDyck: {
+        Sabrina: {},
+        Michelle: {},
+        Josh: {}
+      },
+      Swain: {
+        Blanch: {},
+        Tom: {},
+        Joe: {}
+      }
+    },
+    Sandberg: {
+      Goler: {
+        Eddie: {},
+        Julie: {},
+        Annie: {}
+      },
+      Hernandez: {
+        Rowi: {},
+        Inga: {},
+        Morgan: {}
+      },
+      Moissinac: {
+        Amy: {},
+        Chuck: {},
+        Vinni: {}
+      },
+      Kelley: {
+        Eric: {},
+        Ana: {},
+        Wes: {}
+      }
+    }
+  }
+};
 
-  let sum = 0
-  for (let i = n; i > 0; i--) {
-    sum += i
+const displayOrgChart = (orgChart) => {
+
+  let results = [];
+
+  // General case
+  for (let member in orgChart) {                          //{1} "member" equals all objects in orgChart.
+    if (typeof orgChart[member] === "object") {           //{2} If the values in orgChart are objects,
+      let subKeys = displayOrgChart(orgChart[member]);    //{3} send the objects through the displayOrgChart function again as subKeys.
+      results = results.concat(subKeys.map(subKey => {    //{4} Map through all the subKeys and add "  " to the beginning and add them to the array results.
+        return "  " + subKey;
+      }))
+    } else {
+      results.push(orgChart[member]);                     //{5} If "member" is not an object, push it to results. This will not add spaces to the beginning of them.
+    }
   }
 
-  return sum
-}
+  // Base case
+  return results;
+};
 
-console.log(tri(3))
+console.log(displayOrgChart(orgChart));
